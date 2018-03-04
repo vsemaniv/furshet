ec9f772d8b72558e6dc18cf7c86a267a6048274f
Volodymyr Semaniv <semaniv.volodymyr@gmail.com>
2018-03-04 18:45:32 +0200

    Add google marker

--- js/front.js
+++ js/front.js
@@ -56,8 +56,8 @@ function map() {
     var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
     map = new GMaps({
         el: '#map',
-        lat: -12.043333,
-        lng: -77.028333,
+        lat: 48.285723,
+        lng: 25.940043,
         zoomControl: true,
         zoomControlOpt: {
             style: 'SMALL',
@@ -75,8 +75,8 @@ function map() {
     var image = 'img/marker.png';
 
     map.addMarker({
-        lat: -12.043333,
-        lng: -77.028333,
+        lat: 48.285723,
+        lng: 25.940043,
         icon: image/* ,
          title: '',
          infoWindow: {
